88e53e7f9079ef4e74a13352eb48694c391ffaf2
Rao Hamza Tariq <raohamza6767@gmail.com>
2024-12-07 21:39:57 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ export default function Home() {
   return (
     <>
     <div>
-      <h1>Home</h1>
+      <h1 className="font-poppins">Home</h1>
+      <p className="font-roboto">ksnfdfmd
+        ldsfs
+      </p>
+      <p className="font-sans">dfsdfsd </p>
     </div>
     </>
   );
